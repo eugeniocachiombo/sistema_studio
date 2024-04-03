47a8da302b189bd86278ce3b1f7ed10f6ba0e476
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-04-03 01:09:31 +0100

    Pequisando rotas de modo offiline

--- resources/views/livewire/inclusao/pesquisa.blade.php
+++ resources/views/livewire/inclusao/pesquisa.blade.php
@@ -1,34 +1,10 @@
-<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-<style>
-    .ui-autocomplete {
-        max-height: 100px;
-        overflow-y: auto;
-        overflow-x: hidden;
-        position: absolute;
-        z-index: 9999;
-    }
-
-    * html .ui-autocomplete {
-        height: 100px;
-    }
-
-    .description {
-        font-size: 0.8em;
-        color: rgb(0, 0, 0); /* Cor do texto da descrição */
-        background-color: transparent; /* Fundo transparente para não alterar a cor de fundo do item */
-        margin-top: 5px;
-    }
-
-    .ui-autocomplete li.ui-state-hover {
-        background-color: transparent !important; /* Evitar mudança de cor de fundo ao passar o mouse */
-    }
-</style>
-
+<link rel="stylesheet" href="{{asset("assets/autocompleteOffline/jquery-ui.css")}}">
+<script src="{{asset("assets/autocompleteOffline/jquery-3.6.0.js")}}"></script>
+<script src="{{asset("assets/autocompleteOffline/jquery-ui.js")}}"></script>
+<link rel="stylesheet" href="{{asset("assets/autocompleteOffline/estilo.css")}}">
 <script>
     $(function() {
-        var availableTags = [
+        var rotasDisponiveis = [
             {
                 label: "Página Inicial",
                 value: "Página Inicial",
@@ -44,7 +20,7 @@
         ];
 
         $("#campoPesquisa").autocomplete({
-            source: availableTags,
+            source: rotasDisponiveis,
             select: function(event, ui) {
                 window.location.href = ui.item.route;
             }
@@ -53,7 +29,7 @@
                 .append("<div class='description'>" + item.label + " <span style='font-size:1.0em; color: #999'>" + item
                     .description + "</span></div>")
                 .appendTo(ul);
-        };;
+        };
     });
 </script>
 <div class="search-bar">
@@ -64,3 +40,5 @@
         </form>
     </div>
 </div>
+
+
