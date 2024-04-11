d8dc1ec68958c124f06f17c0ced3bbfc219e5980
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-04-11 12:15:23 +0100

    Opção de carregar arquivo implementado

--- app/Http/Livewire/Chat/Conversa.php
+++ app/Http/Livewire/Chat/Conversa.php
@@ -3,12 +3,28 @@
 namespace App\Http\Livewire\Chat;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Conversa extends Component
 {
+    use WithFileUploads;
+    public $habilitarUpload = false;
+    public $arquivo;
+
     public function render()
     {
         return view('livewire.chat.conversa');
+    }
+
+    public function habilitarInputFile(){
+        if($this->habilitarUpload == true){
+            $this->habilitarUpload = false;
+        }else{
+            $this->habilitarUpload = true;
+        }
+    }
+
+    public function enviarMensagem(){
     }
 
     public function index()
--- resources/views/livewire/chat/conversa.blade.php
+++ resources/views/livewire/chat/conversa.blade.php
@@ -10,43 +10,70 @@
                     </ol>
                 </nav>
             </div>
-    
+
             <section class="section contact">
                 <div class="row gy-4">
                     <div class="col-xl-6">
                         <div class="card card-animated p-4">
                             <caption>
-                                <h4> <i class="bi bi-cursor-fill text-primary"></i> <b class="text-primary">Nome do utilizador</b> </h4>
+                                <h4> <i class="bi bi-cursor-fill text-primary"></i> <b class="text-primary">Nome do
+                                        utilizador</b> </h4>
                             </caption>
                             <hr>
-                            <form wire:submit.prevent="enviarEmail" class="php-email-form needs-validation" novalidate>
+                            <form wire:submit.prevent="enviarMensagem" class="php-email-form needs-validation"
+                                novalidate>
                                 <div class="row gy-4">
-                                    <div class="col text-center">
-                                        <b class="">Não existe conversa com este utilizador</b>
+                                    <div class="col d-table">
+                                        <div class="col text-center">
+                                            <b class="">Não existe conversa com este utilizador</b>
+                                        </div>
+
+                                        @if ($this->habilitarUpload == true)
+                                            <div
+                                                class="col bg-primary aling-items-center d-flex justify-content-between mt-4 border">
+                                                <div class="col">
+                                                    <label for="file-input" class="file-input">
+                                                        Escolha um arquivo
+                                                        <input type="file" wire:model="arquivo" name="file"
+                                                            id="file-input" >
+                                                    </label>
+                                                </div>
+
+                                                <div class="col  d-flex  align-items-center">
+                                                    <span class="text-light"
+                                                        id="nomeArquivo">{{ $arquivo ? $arquivo->getClientOriginalName() : 'Nenhum arquivo escolhido' }}</span>
+                                                </div>
+                                            </div>
+                                        @endif
                                     </div>
-    
+
                                     <div class="col-md-12">
-                                        <textarea class="form-control" rows="5" wire:model="mensagem" rows="6" placeholder="Escreva sua mensagem aqui" required></textarea>
+                                        <textarea class="form-control" rows="5" wire:model="mensagem" rows="6"
+                                            placeholder="Escreva sua mensagem aqui" required></textarea>
                                         <div class="text-danger pt-2" style="font-size: 12.5px">
                                             @error('mensagem')
                                                 <span class="error">{{ $message }}</span>
                                             @enderror
                                         </div>
                                     </div>
-    
+
                                     <div class="col-md-12 text-center d-flex">
                                         <div class="col">
-                                            BtN upload
+                                            <button class="btn btn-primary btn-sm" type="submit"
+                                                wire:click='habilitarInputFile'>
+                                                <i class="bi bi-upload "></i> Carregar arquivo
+                                            </button>
                                         </div>
-                                        
+
                                         <div class="col">
-                                            <span class="text-primary" style="font-size: 20px" wire:loading wire:target='enviarEmail'>
+                                            <span class="text-primary" style="font-size: 20px" wire:loading
+                                                wire:target='enviarMensagem'>
                                                 <span class="spinner-border spinner-border-sm"></span>
-                                                    Processando...
+                                                Processando...
                                             </span>
-                                            <button type="submit" wire:loading.attr='disabled' 
-                                                wire:loading.remove wire:target='enviarEmail'>
-                                                <i class="bi bi-cursor-fill text-light"></i>  Enviar
+                                            <button class="btn-primary" type="submit" wire:loading.attr='disabled'
+                                                wire:loading.remove wire:target='enviarMensagem'>
+                                                <i class="bi bi-cursor-fill text-light"></i> Enviar
                                             </button>
                                         </div>
                                     </div>
@@ -56,7 +83,7 @@
                     </div>
                 </div>
             </section>
-    
+
         </main>
     </div>
 </div>
