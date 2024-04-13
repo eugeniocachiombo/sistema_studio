d21bd9c4e7c0586c65b6c92fb16eccd6e3d744b9
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-04-14 00:09:26 +0100

    Listando as datas com os dias de semanas e mes em portugues

--- app/Http/Livewire/Chat/Conversa.php
+++ app/Http/Livewire/Chat/Conversa.php
@@ -221,25 +221,45 @@ class Conversa extends Component
 
     function formatarData($data) {
         $data_hora = new DateTime($data);
-        $meses = array(
-            'January' => 'Janeiro',
-            'February' => 'Fevereiro',
-            'March' => 'Março',
-            'April' => 'Abril',
-            'May' => 'Maio',
-            'June' => 'Junho',
-            'July' => 'Julho',
-            'August' => 'Agosto',
-            'September' => 'Setembro',
-            'October' => 'Outubro',
-            'November' => 'Novembro',
-            'December' => 'Dezembro'
-        );
-        $data_formatada = $data_hora->format('d \d\e F \d\e Y \á\s H:i:s - ');
-        $data_formatada = strtr($data_formatada, $meses);
-        $data_formatada = str_replace('dUTC', 'de', $data_formatada);
+        $agora = new DateTime('now');
+        $diferenca = $data_hora->diff($agora)->days;
+    
+        if ($diferenca == 0) {
+            $data_formatada = 'Hoje às ' . $data_hora->format('H:i:s');
+        } elseif ($diferenca == 1) {
+            $data_formatada = 'Ontem às ' . $data_hora->format('H:i:s');
+        } elseif ($diferenca >= 2 && $diferenca <= 6) {
+            $dias_semana = array(
+                'Sunday' => 'Domingo',
+                'Monday' => 'Segunda-feira',
+                'Tuesday' => 'Terça-feira',
+                'Wednesday' => 'Quarta-feira',
+                'Thursday' => 'Quinta-feira',
+                'Friday' => 'Sexta-feira',
+                'Saturday' => 'Sábado'
+            );
+            $data_formatada = $data_hora->format('l \à\s H:i:s');
+            $data_formatada = strtr($data_formatada, $dias_semana);
+        } elseif ($diferenca >= 7) {
+            $meses = array(
+                'January' => 'Janeiro',
+                'February' => 'Fevereiro',
+                'March' => 'Março',
+                'April' => 'Abril',
+                'May' => 'Maio',
+                'June' => 'Junho',
+                'July' => 'Julho',
+                'August' => 'Agosto',
+                'September' => 'Setembro',
+                'October' => 'Outubro',
+                'November' => 'Novembro',
+                'December' => 'Dezembro'
+            );
+            $data_formatada = $data_hora->format('d \d\e F \d\e Y \à\s H:i:s');
+            $data_formatada = strtr($data_formatada, $meses);
+        }
         return $data_formatada;
-    }  
+    }    
 
     public function index($utilizador, $remente)
     {
