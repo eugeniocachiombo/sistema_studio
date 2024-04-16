87529bef262144f4c97c8ca4dd036217a9f2bc4f
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-04-16 22:40:33 +0100

    Listando todos clientes na homepage

--- app/Http/Livewire/PaginaInicial/PaginaInicial.php
+++ app/Http/Livewire/PaginaInicial/PaginaInicial.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire\PaginaInicial;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class PaginaInicial extends Component
@@ -9,7 +11,15 @@ class PaginaInicial extends Component
     public $gravacao;
     public $mixagem;
     public $masterizacao;
-    //public $listeners = ['tempoRealMensagens'];
+    public $utilizador_id;
+    public $utilizadorLogado;
+    public $listaClientes = array();
+
+    public function mount()
+    {
+        $this->listaClientes = User::where("tipo_acesso", 3)->get();
+        $this->utilizador_id = Auth::user()->id;
+    }
 
     public function index()
     {
@@ -18,10 +28,12 @@ class PaginaInicial extends Component
 
     public function render()
     {
+        $this->utilizadorLogado = $this->buscarDadosUtilizador($this->utilizador_id);
         return view('livewire.pagina-inicial.pagina-inicial');
     }
 
-    /*public function tempoRealMensagens(){
-
-    }*/
+    public function buscarDadosUtilizador($id)
+    {
+        return User::find($id);
+    }
 }
--- resources/views/livewire/pagina-inicial/pagina-inicial.blade.php
+++ resources/views/livewire/pagina-inicial/pagina-inicial.blade.php
@@ -219,7 +219,8 @@
                             </div>
                         </div>
 
-
+                        {{--Clientes--}}
+                        @if ($utilizadorLogado->tipo_acesso != 3)
                         <div class="col-12">
                             <div class="card card-animated recent-sales overflow-auto">
                                 <div class="filter">
@@ -243,58 +244,30 @@
                                         <thead>
                                             <tr>
                                                 <th scope="col">#</th>
-                                                <th scope="col">Customer</th>
-                                                <th scope="col">Product</th>
-                                                <th scope="col">Price</th>
-                                                <th scope="col">Status</th>
+                                                    <th scope="col">Nome Completo</th>
+                                                    <th scope="col">Tipo de Acesso</th>
+                                                    <th scope="col">Morada</th>
+                                                    <th scope="col">Estado</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <th scope="row"><a href="#">#2457</a></th>
-                                                <td>Brandon Jacob</td>
-                                                <td><a href="#" class="text-primary">At praesentium minu</a>
-                                                </td>
-                                                <td>$64</td>
-                                                <td><span class="badge bg-success">Approved</span></td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row"><a href="#">#2147</a></th>
-                                                <td>Bridie Kessler</td>
-                                                <td><a href="#" class="text-primary">Blanditiis dolor omnis
-                                                        similique</a></td>
-                                                <td>$47</td>
-                                                <td><span class="badge bg-warning">Pending</span></td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row"><a href="#">#2049</a></th>
-                                                <td>Ashleigh Langosh</td>
-                                                <td><a href="#" class="text-primary">At recusandae
-                                                        consectetur</a></td>
-                                                <td>$147</td>
-                                                <td><span class="badge bg-success">Approved</span></td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row"><a href="#">#2644</a></th>
-                                                <td>Angus Grady</td>
-                                                <td><a href="#" class="text-primar">Ut voluptatem id earum
-                                                        et</a></td>
-                                                <td>$67</td>
-                                                <td><span class="badge bg-danger">Rejected</span></td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row"><a href="#">#2644</a></th>
-                                                <td>Raheem Lehner</td>
-                                                <td><a href="#" class="text-primary">Sunt similique
-                                                        distinctio</a></td>
-                                                <td>$165</td>
-                                                <td><span class="badge bg-success">Approved</span></td>
-                                            </tr>
+                                            @foreach ($listaClientes as $item)
+                                                <tr>
+                                                    <th scope="row"><a href="#">{{$item->id}}</a></th>
+                                                    <td>{{$item->name}}</td>
+                                                    <td>
+                                                        {{ucwords($item->buscarTipoAcesso->tipo)}}
+                                                    </td>
+                                                    <td>Morada</td>
+                                                    <td><span class="badge bg-success">Approved</span></td>
+                                                </tr>
+                                                @endforeach
                                         </tbody>
                                     </table>
                                 </div>
                             </div>
                         </div>
+                        @endif
                     </div>
                 </div>
 
