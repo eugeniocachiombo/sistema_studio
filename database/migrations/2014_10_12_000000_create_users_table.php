9233e292d0fd5f2c826bcea03b4e28bf7a603604
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-04-24 18:13:25 +0100

    coluna telefone adicionado ao utilizador

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('telefone')->unique()->nullable();
             $table->unsignedBigInteger('tipo_acesso')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
