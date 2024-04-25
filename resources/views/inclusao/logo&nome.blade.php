6e514480d2fa6ef3a797b6f8bd9e50ee7b859cb6
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-04-25 17:59:12 +0100

    Sistema com logotipo comercial

--- resources/views/inclusao/logo&nome.blade.php
+++ resources/views/inclusao/logo&nome.blade.php
@@ -1,7 +1,6 @@
-<div class="d-flex justify-content-center pb-4">
-    <a href="{{ route('pagina_inicial.') }}" class="logo d-flex align-items-center w-auto">
-        <img src="{{ asset('assets/img/logo.png') }}" alt="logo" style=" object-fit: cover;">
-        <span class="d-none d-lg-block">@include('inclusao.nomesite')</span>
+<div class="d-flex justify-content-center pb-4 ">
+    <a href="{{ route('pagina_inicial.') }}" class="d-flex align-items-center w-auto">
+        <img src="{{ asset('assets/img/logo.png') }}" alt="logo" style="width:100px; object-fit: cover;">
     </a>
 </div>
 
