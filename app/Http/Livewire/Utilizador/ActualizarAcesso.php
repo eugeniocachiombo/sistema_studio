ecdf653d395c7fc401d683fe5665af5ca0f11bd1
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-05-18 08:51:33 +0100

    Utilizador Testado

--- app/Http/Livewire/Utilizador/ActualizarAcesso.php
+++ app/Http/Livewire/Utilizador/ActualizarAcesso.php
@@ -4,6 +4,9 @@ namespace App\Http\Livewire\Utilizador;
 
 use App\Models\Acesso\Acesso;
 use App\Models\User;
+use App\Models\Utilizador\RegistroActividade;
+use Illuminate\Support\Facades\Auth;
+use Jenssegers\Agent\Agent;
 use Livewire\Component;
 
 class ActualizarAcesso extends Component
@@ -12,10 +15,12 @@ class ActualizarAcesso extends Component
     public $utilizador_id;
     public $dadosUtilizador;
     public $listaAcessos;
+    public $infoDispositivo;
 
     public function mount($id)
     {
         $this->utilizador_id = $id;
+        $this->buscarDadosDispositivo();
     }
 
     public function index($id)
@@ -33,6 +38,29 @@ class ActualizarAcesso extends Component
     public function actualizarAcesso(){
         User::where("id", $this->utilizador_id)->update(["tipo_acesso" => $this->tipo_acesso_id]);
         $this->emit('alerta', ['mensagem' => 'Acesso do utilizador alterado', 'icon' => 'success', 'tempo' => 5000]);
+        $this->registrarActividade("<b><i class='bi bi-check-circle-fill text-success'></i> Alterou o acesso de " .  User::find($this->utilizador_id)->name . " </b> <hr>" . $this->infoDispositivo, "normal", Auth::user()->id);
         $this->emit('atrazar_redirect', ['caminho' => '/pagina_inicial', 'tempo' => 2500]);
+    }
+
+    public function registrarActividade($msg, $tipo, $user_id)
+    {
+        RegistroActividade::create([
+            "mensagem" => $msg,
+            "tipo_msg" => $tipo,
+            "user_id" => $user_id,
+        ]);
+    }
+
+    public function buscarDadosDispositivo()
+    {
+        $agente = new Agent();
+        $dispositivo = $agente->device();
+        $plataforma = $agente->platform();
+        $versaoPlataforma = $agente->version($plataforma);
+        $navegador = $agente->browser();
+        $versaoNavegador = $agente->version($navegador);
+        $this->infoDispositivo = "<b class='text-primary'>Dispositivo:</b> " . $agente->device() . " <br>" .
+            "<b class='text-primary'>Plataforma:</b> " . $plataforma . " " . $versaoPlataforma . " <br>" .
+            "<b class='text-primary'>Navegador:</b> " . $navegador . " " . $versaoNavegador . " ";
     }
 }
--- app/Http/Livewire/Utilizador/ListaTodos.php
+++ app/Http/Livewire/Utilizador/ListaTodos.php
@@ -4,13 +4,20 @@ namespace App\Http\Livewire\Utilizador;
 
 use App\Models\User;
 use App\Models\Utilizador\FotoPerfil;
+use App\Models\Utilizador\RegistroActividade;
 use DateTime;
 use Illuminate\Support\Facades\Auth;
+use Jenssegers\Agent\Agent;
 use Livewire\Component;
 
 class ListaTodos extends Component
 {
-    public $listaUtilizadores;
+    public $listaUtilizadores, $infoDispositivo;
+
+    public function mount()
+    {
+        $this->buscarDadosDispositivo();
+    }
 
     public function index()
     {
@@ -39,6 +46,7 @@ class ListaTodos extends Component
     }
 
     public function eliminarUtilizador($id){
+        $this->registrarActividade("<b><i class='bi bi-check-circle-fill text-success'></i> Eliminou utilizador " .  User::find($id)->name . " </b> <hr>" . $this->infoDispositivo, "normal", Auth::user()->id);
         User::find($id)->delete();
         $this->emit('alerta', ['mensagem' => 'Utilizador eliminado do sistema', 'icon' => 'success', 'tempo' => 5000]);
         $this->emit('atrazar_redirect', ['caminho' => '/utilizador/listagem/todos', 'tempo' => 2500]);
@@ -70,5 +78,27 @@ class ListaTodos extends Component
         $data_formatada = "$dia de $mes de $ano";
         $data_formatada = mb_convert_case($data_formatada, MB_CASE_TITLE, "UTF-8");
         return $data_formatada;
+    }
+
+    public function registrarActividade($msg, $tipo, $user_id)
+    {
+        RegistroActividade::create([
+            "mensagem" => $msg,
+            "tipo_msg" => $tipo,
+            "user_id" => $user_id,
+        ]);
+    }
+
+    public function buscarDadosDispositivo()
+    {
+        $agente = new Agent();
+        $dispositivo = $agente->device();
+        $plataforma = $agente->platform();
+        $versaoPlataforma = $agente->version($plataforma);
+        $navegador = $agente->browser();
+        $versaoNavegador = $agente->version($navegador);
+        $this->infoDispositivo = "<b class='text-primary'>Dispositivo:</b> " . $agente->device() . " <br>" .
+            "<b class='text-primary'>Plataforma:</b> " . $plataforma . " " . $versaoPlataforma . " <br>" .
+            "<b class='text-primary'>Navegador:</b> " . $navegador . " " . $versaoNavegador . " ";
     }
 }
