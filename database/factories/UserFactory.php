19dcf34ab6644d23cd90cefe74183f3d47744781
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-06-10 19:07:13 +0100

    Seed Reparada

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\Acesso\Acesso;
-use App\Models\Participante\Participante;
 use App\Models\User;
 use App\Models\Utilizador\Pessoa;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -19,21 +17,50 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(), 
-            'password' => Hash::make('123456'), 
-            'telefone' => $this->buscarTelefoneAleatorio(), 
-            'tipo_acesso' => 3,
-            'remember_token' => Str::random(10),
-        ];
+        $pessoa_id = $this->buscarIDPessoa();
+        $pessoa = Pessoa::find($pessoa_id);
+        $user = User::where("pessoa_id", $pessoa->id)->first();
+        
+        if ($user) {
+            return [];
+        } else {
+            if ($pessoa) {
+                return [
+                    "pessoa_id" => $pessoa->id,
+                    'name' => $this->faker->name(),
+                    'email' => $this->faker->unique()->safeEmail(),
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('123456'),
+                    'telefone' => $this->buscarTelefoneAleatorio(),
+                    'tipo_acesso' => 3,
+                    'remember_token' => Str::random(10),
+                ];
+
+            } else {
+                return [];
+            }
+        }
     }
 
-    public function buscarTelefoneAleatorio(){
-        $telefone = "9".rand(1, 5).rand(1111111, 9999999);
+    public function buscarIDPessoa()
+    {
+        $pessoas = \App\Models\Utilizador\Pessoa::all();
+        $arrayIDsUsers = array();
+        foreach ($pessoas as $item) {
+            array_push($arrayIDsUsers, $item->id);
+        }
+
+        $qtdUsers = count($pessoas);
+        $indiceUser = $qtdUsers > 0 ? rand(0, ($qtdUsers - 1)) : 0;
+        $pessoa_id = $arrayIDsUsers[$indiceUser];
+        return $pessoa_id;
+    }
+
+    public function buscarTelefoneAleatorio()
+    {
+        $telefone = "9" . rand(1, 5) . rand(1111111, 9999999);
         while (User::where("telefone", $telefone)->first()) {
-            $telefone = "9".rand(1, 5).rand(1111111, 9999999);
+            $telefone = "9" . rand(1, 5) . rand(1111111, 9999999);
         }
         return $telefone;
     }
--- database/factories/Utilizador/PessoaFactory.php
+++ database/factories/Utilizador/PessoaFactory.php
@@ -2,9 +2,6 @@
 
 namespace Database\Factories\Utilizador;
 
-use App\Models\Participante\Participante;
-use App\Models\User;
-use App\Models\Utilizador\Pessoa;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PessoaFactory extends Factory
@@ -16,43 +13,12 @@ class PessoaFactory extends Factory
      */
     public function definition()
     {
-        $id = $this->buscarIDUtilizador();
-        $utilizador = User::find($id);
-        $pessoa = Pessoa::where("user_id", $utilizador->id)->first();
-
-        if ($pessoa) {
-            return [];
-        } else {
-            if ($utilizador) {
-                return [
-                    "nome" => "Conta",
-                    "sobrenome" => "Cantor",
-                    "genero" => $this->faker->randomElement(["M", "F"]),
-                    "nascimento" => rand(1990, 2002)."-". rand(1, 12) ."-". rand(1, 28),
-                    "user_id" => $this->buscarIDUtilizador()
-                ];
-            } else {
-                return [];
-            }
-        }
+        return [
+            "nome" => "Conta",
+            "sobrenome" => "Cantor",
+            "genero" => $this->faker->randomElement(["M", "F"]),
+            "nascimento" => rand(1990, 2002) . "-" . rand(1, 12) . "-" . rand(1, 28),
+        ];
     }
 
-    public function buscarIDUtilizador()
-    {
-        $user = \App\Models\User::select("users.*")
-            ->leftJoin('pessoas', 'users.id', '=', 'pessoas.user_id')
-            ->where("users.tipo_acesso", 3)
-            ->where(function ($query) {
-                $query->whereNull("pessoas.id");
-            })
-            ->get();
-        $arrayIDsUsers = array();
-        foreach ($user as $item) {
-            array_push($arrayIDsUsers, $item->id);
-        }
-        $qtdUsers = count($user);
-        $indiceUser = $qtdUsers > 0 ? rand(0, ($qtdUsers - 1)) : 0;
-        $user_id = $arrayIDsUsers[$indiceUser];
-        return $user_id;
-    }
 }
