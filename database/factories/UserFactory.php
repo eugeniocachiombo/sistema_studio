fbe531abba902c1cfdd5cc316992a33f119675c4
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-06-10 20:05:40 +0100

    Seed reajustado

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,43 +17,23 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        $pessoa_id = $this->buscarIDPessoa();
-        $pessoa = Pessoa::find($pessoa_id);
-        $user = User::where("pessoa_id", $pessoa->id)->first();
+        $pessoa = Pessoa::create([
+            "nome" => "Conta",
+            "sobrenome" => "Cantor",
+            "genero" => $this->faker->randomElement(["M", "F"]),
+            "nascimento" => rand(1990, 2002) . "-" . rand(1, 12) . "-" . rand(1, 28),
+        ]);
         
-        if ($user) {
-            return [];
-        } else {
-            if ($pessoa) {
-                return [
-                    "pessoa_id" => $pessoa->id,
-                    'name' => $this->faker->name(),
-                    'email' => $this->faker->unique()->safeEmail(),
-                    'email_verified_at' => now(),
-                    'password' => Hash::make('123456'),
-                    'telefone' => $this->buscarTelefoneAleatorio(),
-                    'tipo_acesso' => 3,
-                    'remember_token' => Str::random(10),
-                ];
-
-            } else {
-                return [];
-            }
-        }
-    }
-
-    public function buscarIDPessoa()
-    {
-        $pessoas = \App\Models\Utilizador\Pessoa::all();
-        $arrayIDsUsers = array();
-        foreach ($pessoas as $item) {
-            array_push($arrayIDsUsers, $item->id);
-        }
-
-        $qtdUsers = count($pessoas);
-        $indiceUser = $qtdUsers > 0 ? rand(0, ($qtdUsers - 1)) : 0;
-        $pessoa_id = $arrayIDsUsers[$indiceUser];
-        return $pessoa_id;
+        return [
+            "pessoa_id" => $pessoa->id,
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456'),
+            'telefone' => $this->buscarTelefoneAleatorio(),
+            'tipo_acesso' => 3,
+            'remember_token' => Str::random(10),
+        ];
     }
 
     public function buscarTelefoneAleatorio()
